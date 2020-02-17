b121f98f0f89a69d9312868c532b0642eccc2588
lifesign <leocloudy@gmail.com>
2020-02-17 20:57:55 +0800

    format

--- app.here.sspai/index.js
+++ app.here.sspai/index.js
@@ -12,81 +12,79 @@ function updateData() {
     here.setMiniWindow({ title: "Fetching…" })
     here.parseRSSFeed('https://rsshub.app/sspai/matrix')
     .then((feed) => {
+        //basic check
         if (feed.items.length <= 0) {
             return here.setMiniWindow({ title: "No item found." })
         }
-
         if (feed.items.length > LIMIT) {
             feed.items = feed.items.slice(0, LIMIT)
         }
 
-        // console.log(JSON.stringify(feed.items[0]));
-        let readIds = cache.get('readIds');
-        if (readIds == undefined) {
-            console.log("未读列表缓存初始化")
-            readIds = [];
-        } else {
-            readIds = JSON.parse(readIds);
-        }
-        console.log("cachedIDs:" + JSON.stringify(readIds))
-
-        const unreadFeeds = _.filter(feed.items, (item, index) => !_.includes(readIds, getPostId(item.link)))
-        const topFeed = _.head(unreadFeeds)
-
-        console.log(`topFeed: ${topFeed}`)
-
         // Mini Window
-        here.setMiniWindow({
-            onClick: () => { 
-                if (topFeed != undefined && topFeed.link != undefined)  { here.openURL(topFeed.link) } 
-            },
-            title: topFeed == undefined ? '暂无最新文章' : topFeed.title,
-            detail: "少数派文章更新",
-            accessory: {
-                badge: unreadFeeds.length + ""
-            },
-            popOvers: _.map(unreadFeeds,(item, index) => {
-                return {
-                    title: isDebugMode() ? `${index + 1}. ${item.title} PID:` + getPostId(item.link) : `${index + 1}. ${item.title}`,
-                    onClick: () => {
-                        if (item.link != undefined) {
-                            // 目前 here 缓存用法类似全局持久化，重启 here 或者 reload 之后缓存不会消失
-                            // cache post_id
-                            // https://sspai.com/post/58856
-                            // console.log(cache.get('read_ids'))
-                            // console.log(JSON.stringify(cache.get('read_ids')))
-                            let postId = getPostId(item.link)
-                            //filter already cached postId
-                            if (_.indexOf(readIds, postId) == -1) {
-                                console.log(`cache postId:${postId}`)
-                                readIds.push(postId)
-                                console.log(JSON.stringify(readIds))
-                                cache.set('readIds', readIds);
-                            } else {
-                                console.log(`cacheExists:${postId} skip`)
-                            }
+        let renderMiniwindow = () => {
+            // console.log(JSON.stringify(feed.items[0]));
+            let readIds = cache.get('readIds');
+            if (readIds == undefined) {
+                console.log("未读列表缓存初始化")
+                readIds = [];
+            } else {
+                readIds = JSON.parse(readIds);
+            }
+            console.log("cachedIDs:" + JSON.stringify(readIds))
 
-                            // here.openURL(item.link)
-                        }
-                    },
-                }
+            let unreadFeeds = _.filter(feed.items, (item, index) => !_.includes(readIds, getPostId(item.link)))
+            let topFeed = _.head(unreadFeeds)
+
+            console.log(`topFeed: ${topFeed}`)
+
+            here.setMiniWindow({
+                onClick: () => {
+                    if (topFeed != undefined && topFeed.link != undefined)  { here.openURL(topFeed.link) }
+                },
+                title: topFeed == undefined ? '暂无最新文章' : topFeed.title,
+                detail: "少数派文章更新",
+                accessory: {
+                    badge: unreadFeeds.length + ""
+                },
+                popOvers: _.map(unreadFeeds,(item, index) => {
+                    return {
+                        title: isDebugMode() ? `${index + 1}. ${item.title} PID:` + getPostId(item.link) : `${index + 1}. ${item.title}`,
+                        onClick: () => {
+                            if (item.link != undefined) {
+                                // 目前 here 缓存用法类似全局持久化，重启 here 或者 reload 之后缓存不会消失
+                                let postId = getPostId(item.link)
+                                //filter cached postId
+                                if (_.indexOf(readIds, postId) == -1) {
+                                    console.log(`cache postId:${postId}`)
+                                    readIds.push(postId)
+                                    console.log(JSON.stringify(readIds))
+                                    cache.set('readIds', readIds);
+                                } else {
+                                    console.log(`cacheExists:${postId} skip`)
+                                }
+
+                                // here.openURL(item.link)
+                            }
+                        },
+                    }
+                })
             })
-        })
+
+        }
+
+        console.log("render miniwindow start...")
+        renderMiniwindow()
 
         here.onPopOverAppear(() => {
             console.log("onPopOverAppear")
         })
 
-        //移出 popup 的时候 更新未读数量
+        //移出 popup 的时候 重绘 miniwindow，当前 here 不支持 partial render
         here.onPopOverDisappear(() => {
             console.log("onPopOverDisappear")
-            // here.setMiniWindow({
-            //     accessory: {
-            //         badge: '小猫咪'
-            //     },
-            // })
+            console.log("rerender miniwindow start")
+            renderMiniwindow()
         })
-
 
         //dock TODO
 
