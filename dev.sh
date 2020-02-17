049b7beef48e324f953c119c5c828d2f9ddb9067
lifesign <leocloudy@gmail.com>
2020-02-17 16:46:06 +0800

    add dev.sh for local development

---
+++ dev.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# quit here first
+osascript -e 'quit app "Here"'
+
+echo -e "deleting sspai plugin"
+rm -rf ~/Library/Application\ Support/app.here/plugins/app.here.sspai/
+
+echo -e "link to dev version"
+ln -s $(pwd)/app.here.sspai/ ~/Library/Application\ Support/app.here/plugins/app.here.sspai
+
+echo -e "link done.. restart here"
+osascript -e 'tell application "here" to activate'
