f060077d9e6e6a85365eeb6fd07c1d5e973fa1ad
lifesign <leocloudy@gmail.com>
2020-02-24 15:24:43 +0800

    fix: pack.sh

--- pack.sh
+++ pack.sh
@@ -4,6 +4,6 @@ echo -e "packing start"
 echo -e "Clean last build"
 rm -rf *.hereplugin
 echo -e "Compressing to current dir"
-zip -r app.here.sspai.zip app.here.sspai -x *.DS_Store*
+zip -r app.here.sspai.zip src -x *.DS_Store*
 mv app.here.sspai.zip app.here.sspai.hereplugin
 echo -e "packing done.."
