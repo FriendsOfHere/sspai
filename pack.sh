e4e5985a9503e32144eb81b540a7e8eb794895ce
lifesign <leocloudy@gmail.com>
2022-02-07 22:39:08 +0800

    chore: fix pack script

--- pack.sh
+++ pack.sh
@@ -4,6 +4,6 @@ echo -e "packing start"
 echo -e "Clean last build"
 rm -rf *.hereplugin
 echo -e "Compressing to current dir"
-zip -r app.here.sspai.zip src -x *.DS_Store*
+zip -r app.here.sspai.zip . -x ".*"
 mv app.here.sspai.zip SSPai.hereplugin
 echo -e "packing done.."
