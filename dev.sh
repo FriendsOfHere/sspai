7262497b54f1413934eccdf30b4661e6c6270668
lifesign <leocloudy@gmail.com>
2022-02-07 22:27:45 +0800

    chore: update dev script

--- dev.sh
+++ dev.sh
@@ -4,10 +4,10 @@
 osascript -e 'quit app "Here"'
 
 echo -e "deleting sspai plugin"
-rm -rf ~/Library/Application\ Support/app.here/plugins/app.here.sspai/
+rm -rf ~/Library/Application\ Support/app.here/plugins/foh.sspai/
 
 echo -e "link to dev version"
-ln -s $(pwd)/src/ ~/Library/Application\ Support/app.here/plugins/app.here.sspai
+ln -s $(pwd)/src/ ~/Library/Application\ Support/app.here/plugins/foh.sspai
 
 echo -e "link done.. restart here"
 osascript -e 'tell application "here" to activate'
